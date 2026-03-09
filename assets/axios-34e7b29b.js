facdbfebc89a5d08c1aabb70e342f9ee4026791b
Jason Llanes <llanezjaizon@gmail.com>
2026-03-09 16:09:26 +0800

    Updates

---
+++ assets/axios-34e7b29b.js
@@ -0,0 +1,1 @@
+import{a}from"./axios-7a713374.js";a.defaults.baseURL="http://192.168.18.179:8000/api/v1/";a.defaults.headers.get.Accept="application/json";a.defaults.headers.post["Content-Type"]="application/json";
